8673371edc1c54ff64546be907525aa6304e1af3
Vamco2020 <101957077+Vamco2022@users.noreply.github.com>
2024-05-16 21:08:19 +0800

    Delete src directory

--- src/activeFunction.java
+++
@@ -1,9 +0,0 @@
-public class activeFunction {
-    public static double sigmoid(double f){
-        return 1 / (1 + (Math.pow(Math.E, -f)));
-    }
-
-    public static double sigmoidDerivative(double f){
-        return sigmoid(f) * (1 - sigmoid(f));
-    }
-}
